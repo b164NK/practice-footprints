9f50c79ea06b768f6c4e341e66c0499af401dec3
健太郎 <kentarou@kentarou-no-MacBook-Air.local>
2020-09-05 21:29:16 +0900

    task complete

--- touch.js
+++ touch.js
@@ -1,5 +1,8 @@
 window.onload = function(){
-
+	var el_myCanvas = document.getElementById('myCanvas');
+	var clientRect = el_myCanvas.getBoundingClientRect();
+	var positionX = clientRect.left + window.pageXOffset ;
+	var positionY = clientRect.top + window.pageYOffset ;
 
 	//Vueインスタンス
 	new Vue({
@@ -7,16 +10,21 @@ window.onload = function(){
 		data:{
 			pointX: 0,
 			pointY: 0,
-			myText: '画面をタッチしてください'
+			myText: '画面をタッチしてください',
+			bgcolor: 'blue'
 		},
 		methods:{
-			touchStart: function(e){
-			},
 			touchMove: function(e){
 				// タッチによる画面スクロールを止める
 				e.preventDefault();
+				this.myText = 'タッチ中...';
+				this.pointX = e.changedTouches[0].pageX - positionX;
+				this.pointY = e.changedTouches[0].pageY - positionY;
+				this.bgcolor = 'red';
 			},
 			touchEnd: function(e){
+				this.myText = '画面をタッチしてください';
+				this.bgcolor = 'blue';
 			}
 		}
 	});
